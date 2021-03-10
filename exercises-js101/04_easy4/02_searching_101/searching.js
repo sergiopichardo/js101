3ebc7c729132288ee216af2d10c87664b6139400
Sergio Pichardo <sp88work@gmail.com>
2021-03-10 13:17:06 -0600

    add comments to searching.js

---
+++ exercises-js101/04_easy4/02_searching_101/searching.js
@@ -0,0 +1,60 @@
+/*
+Write a program that solicits six numbers from the user,
+then logs a message that describes whether or not the
+sixth number appears amongst the first five numbers.
+*/
+
+
+/**
+ * Get 6 numbers from the user
+ * @param {number}
+ * @return {number[]} - 1, 2, 3, 4, 5
+ */
+
+
+/**
+ * Checks if 6th number is within the first 5 numbers
+ * @param {number[]}
+ * @return {boolean}
+ */
+
+
+/**
+ * Returns a message wether the 6th number appears in
+ * the first 5 numbers
+ * @param {boolean} isNumberInRange
+ * @return {string}
+ * @example - if true: The number 17 appears in 25,15,20,17,23.
+ * @example - if false: The number 18 does not appear in 25,15,20,17,23.
+ */
+
+
+
+
+
+
+
+
+
+
+/*
+Enter the 1st number: 25
+Enter the 2nd number: 15
+Enter the 3rd number: 20
+Enter the 4th number: 17
+Enter the 5th number: 23
+Enter the last number: 17
+
+The number 17 appears in 25,15,20,17,23.
+
+-----
+
+Enter the 1st number: 25
+Enter the 2nd number: 15
+Enter the 3rd number: 20
+Enter the 4th number: 17
+Enter the 5th number: 23
+Enter the last number: 18
+
+The number 18 does not appear in 25,15,20,17,23.
+*/
