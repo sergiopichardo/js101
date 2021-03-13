0e3be7a15ee7b20c23b23259467dfdf54fc8cabc
Sergio Pichardo <sp88work@gmail.com>
2021-03-13 15:11:56 -0600

    add solution for stringToNumber.js exercise

---
+++ exercises-js101/02_easy2/09_convert_a_string_to_a_number/stringToNumber/stringToNumber.v1.js
@@ -0,0 +1,16 @@
+/*
+Convert a String to a Number
+Author: Sergio Pichardo
+Date: March 13, 2021
+*/
+
+/**
+ * Converts a numeric string into an integer
+ * @param {string} numericString
+ * @return {number}
+ */
+const stringToInteger = numericString => +(numericString)
+
+
+console.log(stringToInteger("4321") === 4321); // logs true
+console.log(stringToInteger("570") === 570); // logs true
