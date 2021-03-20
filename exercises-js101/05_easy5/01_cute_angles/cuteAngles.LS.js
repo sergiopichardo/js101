f4ae4a9e5ea358adca808e282bb98d8bcaf180af
Sergio Pichardo <sp88work@gmail.com>
2021-03-19 19:52:07 -0500

    add LS solutions for cuteAngles exercise

---
+++ exercises-js101/05_easy5/01_cute_angles/cuteAngles.LS.js
@@ -0,0 +1,28 @@
+const DEGREE = '\xB0';
+const MINUTES_PER_DEGREE = 60;
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE;
+
+function dms(degreesFloat) {
+  let degreesInt = Math.floor(degreesFloat);
+  let minutes = Math.floor((degreesFloat - degreesInt) * MINUTES_PER_DEGREE);
+  let seconds = Math.floor(
+    (degreesFloat - degreesInt - (minutes / MINUTES_PER_DEGREE)) *
+    SECONDS_PER_DEGREE
+  );
+
+  return String(degreesInt) + DEGREE + padZeroes(minutes) + "'" +
+                                       padZeroes(seconds) + '"';
+}
+
+function padZeroes(number) {
+  let numString = String(number);
+  return numString.length < 2 ? ('0' + numString) : numString;
+}
+
+console.log(dms(30));           // 30°00'00"
+console.log(dms(76.73));        // 76°43'48"
+console.log(dms(254.6));        // 254°35'59"
+console.log(dms(93.034773));    // 93°02'05"
+console.log(dms(0));            // 0°00'00"
+console.log(dms(360));          // 360°00'00" or 0°00'00"
