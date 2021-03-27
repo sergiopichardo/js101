4fe2a5673176324a64095e45daf40cd231aa1cc8
Sergio Pichardo <sp88work@gmail.com>
2021-03-27 17:21:54 -0500

    update fibonacci number while condition

--- exercises-js101/03_easy3/04_fibonacci_number_location_by_length/fibonacciNumber.js
+++ exercises-js101/03_easy3/04_fibonacci_number_location_by_length/fibonacciNumber.js
@@ -10,7 +10,7 @@ function findFibonacciIndexByLength(fibLength) {
   let fibNumber = 0n;
   let fibDigitCount = 0n;
 
-  while (fibDigitCount < fibLength) {
+  while (fibDigitCount !== fibLength) {
     fibNumber = prev1 + prev2;
     prev1 = prev2;
     prev2 = fibNumber;
@@ -24,11 +24,12 @@ function findFibonacciIndexByLength(fibLength) {
 
 
 
-console.log(findFibonacciIndexByLength(2n) === 7n);    // 1 1 2 3 5 8 13
+// console.log(findFibonacciIndexByLength(1n));    // 1 1 2 3 5 8 13
+// console.log(findFibonacciIndexByLength(2n) === 7n);    // 1 1 2 3 5 8 13
 
-console.log(findFibonacciIndexByLength(3n) === 12n);   // 1 1 2 3 5 8 13 21 34 55 89 144
-console.log(findFibonacciIndexByLength(10n) === 45n);
-console.log(findFibonacciIndexByLength(16n) === 74n);
+// console.log(findFibonacciIndexByLength(3n) === 12n);   // 1 1 2 3 5 8 13 21 34 55 89 144
+// console.log(findFibonacciIndexByLength(10n) === 45n);
+// console.log(findFibonacciIndexByLength(16n) === 74n);
 // console.log(findFibonacciIndexByLength(100n) === 476n);
 // console.log(findFibonacciIndexByLength(1000n) === 4782n);
 // console.log(findFibonacciIndexByLength(10000n) === 47847n);
